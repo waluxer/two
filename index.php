3bcc319ce0c7835ed2e13fc76a16b68fbc18c534
AbrahamIsaakovich <ulugbekkaizmov888@gmail.com>
2022-12-20 11:00:34 +0500

    second

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
 <?php
 
-require "vendor/electrolinux/phpquery/phpQuery/phpQuery.php";
-
-$ch = curl_init("https://texmart.kg/new-design/catalog?type=product");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-$html = curl_exec($ch);
-curl_close($ch);
-
-$pq = phpQuery::newDocument($html);
-$text = $pq("h6") -> html();
-echo $text
-
-?>
+?> 
